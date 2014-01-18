dea104856546147fde895de0ed84e9a3f69eba4b
Lee McAlilly <leemcalilly@gmail.com>
2014-01-18 11:46:03 -0600

    setting up rails

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'unicorn'
+gem 'foreman'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0'
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem "capybara", '~> 2.1.0'
+  gem 'rspec-rails', '~> 2.13.1'
+  gem 'launchy', '~> 2.3.0'
+  gem 'database_cleaner', '~> 1.0.1'
+  gem "factory_girl_rails", '~> 4.2.1'
+  gem "guard-rspec", '~> 3.0.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcalilly::Application.config.secret_key_base = '6d999afd7a16aed0011692a5a1b87d76dbc61805897020c0012aba8716c6da82fca458a4bb29a20c73cbf9db01743dd04d3d843d4176bb0e9aa6b7deee91bd22'
