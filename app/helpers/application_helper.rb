4ffab571112842a50a3f4578a233623fc8f4de90
Lee McAlilly <leemcalilly@gmail.com>
2014-01-19 18:05:51 -0600

    finish initial homepage setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def app_name
+    "Lee McAlilly Is Using Technology"
+  end
+  
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "#{app_name}"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} < #{base_title}"
+    end
+  end
+  
 end
