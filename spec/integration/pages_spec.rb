7d5fbe17e54daa0a577d8d12d19ed01cf0d4d199
Lee McAlilly <leemcalilly@gmail.com>
2014-01-21 19:53:26 -0700

    adding content to the resume page

--- spec/integration/pages_spec.rb
+++ spec/integration/pages_spec.rb
@@ -24,7 +24,7 @@ describe "Public" do
     end
     
     it "has the right content" do
-      page.should have_content("Lee McAlilly")
+      page.should have_content("Look out honey cause I'm using technology.")
     end
   end
   
@@ -33,7 +33,7 @@ describe "Public" do
       visit '/resume'
     end
     
-    it "renders the home page" do
+    it "renders the resume page" do
       page.status_code.should be(200)
     end
     
@@ -47,6 +47,21 @@ describe "Public" do
 
     it "has the right body id" do
       page.should have_css('body#resume')
+    end
+    
+    it "has the right links" do
+      page.should have_link("Email")
+      page.should have_link("Github")
+      page.should have_link("Twitter")
+      page.should have_link("Instagram")
+      page.should have_link("Original Fuzz")
+    end
+    
+    it "has the right content" do
+      page.should have_content("Experience")
+      page.should have_content("Education")
+      page.should have_content("Web Projects")
+      page.should have_content("Contact")
     end
   end
 
