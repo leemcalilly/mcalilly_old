0ed872ffa1d85040596dd43106f2ef7e63f2e517
Lee McAlilly <leemcalilly@gmail.com>
2014-01-28 22:04:34 -0600

    randomly rotating images on the homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     "Lee McAlilly"
   end
   
+  def random_featured_image
+    ["all-stars", "bass", "fuzz-bag", "microphone", "music-man", "pedals", "plug-in", "tube-screamer"].sample
+  end
+  
   # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "#{app_name}"    
